0ebd83106b62c30671bb3da6f441382d5a55b72d
kumohakasemk9 <None>
2023-08-14 20:45:23 +0900

    fix

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"kumotechmadlab.sffreader","l":"KumoSFFReader"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"kumotechmadlab.sffreader","l":"KumoSFFReader"},{"p":"kumotechmadlab.sffreader","l":"SFFDecodeException"}];updateSearchResults();
